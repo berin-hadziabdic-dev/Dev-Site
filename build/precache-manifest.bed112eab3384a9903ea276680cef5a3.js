046b22b801548a966628078f2aa4d3e7d1aa6d7b
Berin <berin2@pdx.edu>
2020-08-16 15:38:20 -0700

    Added post and pre buildscripts to package.json of react. Added, built version of app to be served from EB express app.

---
+++ build/precache-manifest.bed112eab3384a9903ea276680cef5a3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb81a10f733dd456eca84c35d1fbbc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "add77b8bea1e2bc0c7b1",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "af499b86a08e7af20960",
+    "url": "/static/css/main.fcaa42b7.chunk.css"
+  },
+  {
+    "revision": "add77b8bea1e2bc0c7b1",
+    "url": "/static/js/2.c874cd1b.chunk.js"
+  },
+  {
+    "revision": "a8eb76e4c6beb72c22bcc9f3c026930b",
+    "url": "/static/js/2.c874cd1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af499b86a08e7af20960",
+    "url": "/static/js/main.582e6dab.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  },
+  {
+    "revision": "a45650e5def6185b6f242a0995b1aacb",
+    "url": "/static/media/HIBE.a45650e5.png"
+  },
+  {
+    "revision": "7f09e927c9730177f8d80d1fd8b3d006",
+    "url": "/static/media/PCC.7f09e927.png"
+  },
+  {
+    "revision": "4e82b19745c3f71f4d0491a36b88c03c",
+    "url": "/static/media/PSU.4e82b197.jpg"
+  },
+  {
+    "revision": "8fcf6bd64bdbbcb530b6c726804bbf50",
+    "url": "/static/media/Serv_b.8fcf6bd6.png"
+  },
+  {
+    "revision": "c2865610e43098257aa2c88716ecd628",
+    "url": "/static/media/banner.c2865610.png"
+  },
+  {
+    "revision": "e2e24bb10e5313eab39327a54a40e160",
+    "url": "/static/media/bootstrap_icon.e2e24bb1.png"
+  },
+  {
+    "revision": "160e1133a0b8d749aa89ccf1a88584b8",
+    "url": "/static/media/generatedtext.160e1133.jpg"
+  },
+  {
+    "revision": "c3658e9262c5a591a77ec3749f920597",
+    "url": "/static/media/mongodb_icon.c3658e92.png"
+  },
+  {
+    "revision": "730c02227ba814373f440485074a53aa",
+    "url": "/static/media/node_icon.730c0222.png"
+  },
+  {
+    "revision": "699d09413a09eb8b10ad54c424df37ee",
+    "url": "/static/media/react_icon.699d0941.png"
+  }
+]);
