60fb5b3fd5cd7df98224d23fdadb9f28dd617387
Berin <berin2@pdx.edu>
2020-08-16 14:40:27 -0700

    Moved files to the root directory to fix EB error on AWS.

---
+++ server.js
@@ -0,0 +1,62 @@
+let express = require("express");
+let server = express();
+let collectionRef = null;
+let { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://BerinHadziabdic:aFlK0cKoPRP2RYgG@cluster0-dzpnn.mongodb.net/<devsite>?retryWrites=true&w=majority";
+let path = require("path");
+
+server.use(express.json());
+
+function saveBody(body) {
+  MongoClient.connect(uri, function (err, dbObj) {
+    if (err === null)
+      dbObj
+        .db("devsite")
+        .collection("messages")
+        .insert({ email: body.email, msg: body.msg }, function (
+          insertionErr,
+          result
+        ) {
+          if (insertionErr === null) {
+            console.log(`Insertion Err Value: ${err}`);
+            console.log(result);
+          } else console.log("There was an error.");
+        });
+  });
+}
+
+function validateBody(body) {
+  let notFalsyBody = body !== null && body !== undefined;
+  let emailMsg = notFalsyBody ? body.email && body.msg : false;
+  let typeCheck =
+    typeof body === "object" &&
+    typeof body.email === "string" &&
+    typeof body.msg === "string";
+
+  return body;
+}
+
+function sanitizeUserInput(body) {
+  let badInput = /[><="'\${:\/}\[\]]/;
+  let returnBody = {};
+  returnBody.email = body.email.replace(badInput, " ");
+  returnBody.msg = body.msg.replace(badInput, " ");
+
+  return returnBody;
+}
+
+server.post("/xl1p1-z?", (req, res) => {
+  let { body } = req.body;
+
+  if (validateBody(body)) {
+    let sanitizedInput = sanitizeUserInput(body);
+  } else {
+    res.status(400).end();
+  }
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
+server.listen(8089, console.log("Listening"));
